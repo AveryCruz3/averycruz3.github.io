890a93a12faa5c9c9fd35d8e1268316d68c230f1
AveryCruz3 <cruzavery60@gmail.com>
2025-09-30 17:58:18 +0000

    created loops to draw multiple circles and move them around and addes background image

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,19 +20,30 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+        var circle; // Holds a single circle when creationg the circles
+        var circles = []; // Variable to hold all the circles in an array
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+        //Code to draw circle
+            circle = draw.randomCircleInArea(canvas ,true,true,'#999',2);// Uses existiong fuction to draw a cirlce of randome size,colro,and location on the cavas.It stores the output of the fuction
+            physikz.addRandomVelocity(circle,canvas);// Uses physkiz libray to add random velocity and direction to the circle
+            view.addChild(circle);//Adds the circle as a child to view so that the cirl appers on the screen
+            circles.push(circle);//Saves the circle to the array of circles,pushing it to the end of the array
+        }
 
 
         // TODO 3 : Call the drawCircle() function
-
-
+        //drawCircle();//Crrating the first circle
+       // drawCircle();//Creating the second circle
+       // drawCircle();//Making the third cirlce
+       // drawCircle();//Making the fourth cirlce
+        //drawCircle();//Summoning the fifth circle
 
         // TODO 7 : Use a loop to create multiple circles
-
+        for(var i =0; i < 100; i++){
+            drawCircle(i)
+        }//Creates a loop that will draw the circle fuction over and over again
 
 
 
@@ -47,14 +58,22 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-
-            
+            //physikz.updatePosition(circles[0])//Moving the position of the first circle 
+            //physikz.updatePosition(circles[1])//moving the position of the seccond circle 
+            //physikz.updatePosition(circles[2])//moving the position of the thrid circle
+            //physikz.updatePosition(circles[3])//moving the position of the fourth cirlce
+            //physikz.updatePosition(circles[4])//moving the position of the fifth circle
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           
-
+            //game.checkCirclePosition(circles[0])//Checks the position of the circles and uses bracket notation to accsess the frist cirlce 
+            //game.checkCirclePosition(circles[1])//Checks the position of the circles and uses bracket notation to accsess the second cirlce
+            //game.checkCirclePosition(circles[2])//Checks the position of the circles and uses bracket notation to accsess the third cirlce
+            //game.checkCirclePosition(circles[3])//Checks the position of the circles and uses bracket notation to accsess the fourth cirlce
+            //game.checkCirclePosition(circles[4])//Checks the position of the circles and uses bracket notation to accsess the fifth cirlce
             // TODO 8 / TODO 9 : Iterate over the array
-           
-            
+            for(var i = 0; i < circles.length; i++){
+                physikz.updatePosition(circles[i])
+                game.checkCirclePosition(circles[i])
+            }//creates a loop that will update the positions and moves the circles over and over again
         }
     
         /* 
@@ -70,9 +89,15 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
-
-
+            if( circle.x < 0){
+                circle.x = canvas.width;
+            }
+            if( circle.y < 0){
+                circle.y = canvas.hight;
+            }
+            if( circle.y > canvas.hight){
+                circle.y = 0;
+            }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
